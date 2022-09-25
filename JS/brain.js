d310e4a19cd6737734ca007ca9a3d6df69efcd7a
shobhit-28 <shobhitraj34@gmail.com>
2022-09-25 05:55:01 +0530

    done

--- JS/brain.js
+++ JS/brain.js
@@ -1,2 +1,23 @@
 // alert("link check");
 
+var input = document.querySelector("textarea[name='input']");
+var btn = document.querySelector(".btn");
+var output = document.querySelector("textarea[name='output']");
+
+function errorHandling(error){
+    alert("There is some error in the server \n" + error);
+}
+
+function clickHandler(){
+    var url = "https://api.funtranslations.com/translate/valyrian.json"
+    txtInput = url + "?" + "text=" + input.value
+    fetch(txtInput)
+        .then(response => response.json())
+        .then(json => {
+            translatedTxt = (json.contents.translated)
+            output.innerText = translatedTxt
+        })
+        .catch(errorHandling)
+}
+
+btn.addEventListener('click', clickHandler);
